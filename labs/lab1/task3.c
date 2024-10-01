4e0bfbbe9cf4fdf01c24e66cd95bf6b74c455709
nothishit <85381545+nothishit@users.noreply.github.com>
2024-10-01 10:21:57 +0500

    Update task3.c

--- labs/lab1/task3.c
+++ labs/lab1/task3.c
@@ -1,25 +1,8 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-int main()
-{
-    char *arr[100];
-    char value[255];
-    int count = 0;
-
-    while (strcmp(value, "stop") != 0)
-    {
-        count++;
-        printf("Введите аргумент %i: ", count);
-        scanf("%s", value);
-        arr[count-1] = malloc(strlen(value) + 1);
-        strcpy(arr[count-1], value);
-    }
-    for (int i = 0; i < count-1; i++)
-    {
-        puts(arr[i]);
-    }
-    free(*arr);
-    return 0;
-}
+ #include <stdio.h>
+ int main (int argc, char * argv[]) {
+     int i;
+     for( i = 1 ; i < argc; i++) {
+        printf("Argument %d: %s\n", i, argv[i]);
+     }
+     return 0;
+ }
